7282083c164620c67272d1b6288b6e848a70cc51
Diogoltt <diogoleite09@gmail.com>
2025-05-06 02:03:10 -0300

    feat: appsettings configs

--- Configuration.cs
+++ Configuration.cs
@@ -2,9 +2,18 @@
 {
     public static class Configuration
     {
-        // Token JWT
         public static string JwtKey = "YVn7z7Q3EgjFl2jYxLBzUFK/3MPh3Ztu5Ds1YTib4x8=";
         public static string ApiKeyName = "api_key";
-        public static string ApiKey = "curso_api_hash";
+        public static string ApiKey = "password_api_hash";
+        public static SmtpConfiguration Smtp = new();
+
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+
+        }
     }
 }
